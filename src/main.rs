2a8f1220e87afca7b973d624ef9f6c822f89e8c2
David Ferrari <davidchristenes13@gmail.com>
2021-12-05 21:02:20 -0300

    One method to create csv without the need of a previous file, but without header

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use csv::Writer;
 use text_io::read;
 use rand::Rng;
 use std::fs::OpenOptions;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 
 fn main() {
     let n : i32 = read!();
@@ -18,14 +21,18 @@ fn generate_data_csv(ntimes: i32) -> Result<(), Box<dyn Error>> {
         .open("data_generated.csv")
         .unwrap();
     let mut wtr = Writer::from_writer(file);
-    let mut vector_of_random_policies: Vec<i32> = Vec::new();
+    let mut vector_of_random_policies: Vec<u8> = Vec::new();
     let mut n = ntimes;
     while n > 0 {
-        vector_of_random_policies.push(rand::thread_rng().gen_range(1..3));
+        vector_of_random_policies.push(rand::thread_rng().gen_range(1..4));
         n -= 1;
     }
-    let string_vector = format!("{:?}", vector_of_random_policies);
-    wtr.serialize(string_vector)?;
+    let mut hash = DefaultHasher::new();
+    vector_of_random_policies.hash(&mut hash);
+    wtr.serialize((
+        format!("{:?}", hash.finish()),
+        format!("{:?}", vector_of_random_policies)
+    ))?;
     wtr.flush()?;
     Ok(())
 }
